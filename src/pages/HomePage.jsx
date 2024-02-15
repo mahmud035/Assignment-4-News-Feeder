59f21e9921dd50fdcb5a397b27d88b22cb597271
Md. Mahamudul Hasan <72532498+mahmud035@users.noreply.github.com>
2024-02-15 08:20:49 +0600

    show Error message

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,17 +2,20 @@ import { useContext } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/header/Header';
 import NewsContainer from '../components/news/NewsContainer';
+import Error from '../components/ui/Error';
 import Loading from '../components/ui/Loading';
 import { NewsContext } from '../contexts/NewsContext';
 
 const HomePage = () => {
-  const { isLoading } = useContext(NewsContext);
+  const { isLoading, isError } = useContext(NewsContext);
 
   return (
     <>
       <Header />
 
       <main className="py-10 transition-all lg:py-14">
+        {isError && <Error />}
+
         {isLoading ? <Loading /> : <NewsContainer />}
       </main>
 
